9724bb240e4178083cd85fab3290f8bec8aac1eb
huos3203 <724987481@qq.com>
2014-06-09 20:51:39 +0800

    rake setup_github_pages git@github.com:huos3203/MyBlog.git

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/MyBlog/"
+http_images_path = "/MyBlog/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/MyBlog/fonts"
+css_dir = "public/MyBlog/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
