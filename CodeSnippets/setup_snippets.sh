868b8babf760f4552361095610039efc8ccc49fa
pengyucheng <pengyucheng@huoshug.local>
2017-01-23 17:33:33 +0800

    Site updated: 2017-01-23 17:33:33

--- CodeSnippets/setup_snippets.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-mv ~/Library/Developer/Xcode/UserData/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets.backup
-
-# rm ~/Library/Developer/Xcode/UserData/CodeSnippets
-#创建替身指向当前目录中的CodeSnippets地址
-SRC_HOME=`pwd`
-ln -s ${SRC_HOME}/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets
-echo "done"
-
