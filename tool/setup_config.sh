3c1c3273d9e6cf390700286c1d882a2b97c1e471
pengyucheng <pengyucheng@huoshug.local>
2017-02-08 16:44:33 +0800

    Site updated: 2017-02-08 16:44:11

--- tool/setup_config.sh
+++ tool/setup_config.sh
@@ -17,17 +17,17 @@ sed -i '' "s/.*maupassant:/#maupassant:/g" _config.yml
 sed -i '' "s/.*jacman:/#jacman:/g" _config.yml
 
 #打印站点名称
+githubURL="https:\/\/huos3203.github.io"
 if [ "$themename" == "jacman" ]; then
     echo "站点：coding"
-    sed -i '' "s/root:.*/root: \//g" _config.yml
 elif [ "$themename" == "next" ]; then
     #主题next
     echo "站点：github"
-    sed -i '' "s/root:.*/root: \//g" _config.yml
+    sed -i '' "s/url:.*/url: ${githubURL}/g" _config.yml
 elif  [ "$themename" == "maupassant" ]; then
     #主题maupassant
-    echo "站点：myblog"
-    sed -i '' "s/root:.*/root: \/boyer\//g" _config.yml
+    echo "站点：github/boyer"
+    sed -i '' "s/url:.*/url: ${githubURL}\/boyer/g" _config.yml
 fi
 #打开发布到对应站点库
 sed -i '' "s/#${themename}:/${themename}:/g" _config.yml
